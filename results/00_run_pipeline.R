3825f2ee614112ca27b2eeb29f820a1cca1b1ee6
Florent Chuffart <florent.chuffart@gmail.com>
2021-06-10 20:55:26 +0200

    V0 of the pipeline

---
+++ results/00_run_pipeline.R
@@ -0,0 +1,9 @@
+rmarkdown::render("01_tss_cpg_status.Rmd")
+rmarkdown::render("02_mapreduce_mean.Rmd")
+rmarkdown::render("03_da_GSE45332.Rmd")
+rmarkdown::render("03_da_GSE5816.Rmd")
+rmarkdown::render("04_results.Rmd")
+
+
+
+
