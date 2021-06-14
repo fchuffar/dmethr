e95f4151adad68571cb95967a551998cf8da2c13
Florent Chuffart <florent.chuffart@gmail.com>
2021-06-14 16:51:09 +0200

    Updating pipeline

--- results/00_run_pipeline.R
+++ results/00_run_pipeline.R
@@ -1,9 +1,22 @@
+step = 100
+ud_str = 2500
+feattrmt = "raw"
+reducer = "mean"
+prefix3 = paste0(reducer, "_", feattrmt, "_", ud_str, "_", step)
+
+rmarkdown::render("01_tss_cpg_status.Rmd", output_file=paste0("01_tss_cpg_status_", prefix3, ".html"))
+rmarkdown::render("02_mapreduce_mean.Rmd", output_file=paste0("02_mapreduce_mean_", prefix3, ".html"))
+rmarkdown::render("03_da_GSE45332.Rmd"   , output_file=paste0("03_da_GSE45332_"   , prefix3, ".html"))
+rmarkdown::render("03_da_GSE5816.Rmd"    , output_file=paste0("03_da_GSE5816_"    , prefix3, ".html"))
+rmarkdown::render("04_results.Rmd"       , output_file=paste0("04_results_"       , prefix3, ".html"))
+
+
+
 rmarkdown::render("01_tss_cpg_status.Rmd")
 rmarkdown::render("02_mapreduce_mean.Rmd")
-rmarkdown::render("03_da_GSE45332.Rmd")
-rmarkdown::render("03_da_GSE5816.Rmd")
-rmarkdown::render("04_results.Rmd")
-
+rmarkdown::render("03_da_GSE45332.Rmd"   )
+rmarkdown::render("03_da_GSE5816.Rmd"    )
+rmarkdown::render("04_results.Rmd"       )
 
 
 
