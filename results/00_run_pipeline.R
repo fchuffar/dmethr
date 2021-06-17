4007ede0ee01ea08be1483b08ad0dc8ca0b7f36c
Florent Chuffart <florent.chuffart@gmail.com>
2021-06-17 09:51:02 +0200

    Update full pipeline

--- results/00_run_pipeline.R
+++ results/00_run_pipeline.R
@@ -1,23 +1,31 @@
-step = 100
-ud_str = 2500
-feattrmt = "raw"
-reducer = "mean"
-prefix3 = paste0(reducer, "_", feattrmt, "_", ud_str, "_", step)
+# step = 100
+# ud_str = 2500
+# feattrmt = "raw"
+# reducer = "mean"
+# prefix3 = paste0(reducer, "_", feattrmt, "_", ud_str, "_", step)
+# rmarkdown::render("01_tss_cpg_status.Rmd", output_file=paste0("01_tss_cpg_status_", prefix3, ".html"))
+# rmarkdown::render("02_mapreduce_mean.Rmd", output_file=paste0("02_mapreduce_mean_", prefix3, ".html"))
+# rmarkdown::render("03_da_GSE45332.Rmd"   , output_file=paste0("03_da_GSE45332_"   , prefix3, ".html"))
+# rmarkdown::render("03_da_GSE5816.Rmd"    , output_file=paste0("03_da_GSE5816_"    , prefix3, ".html"))
+# rmarkdown::render("04_results.Rmd"       , output_file=paste0("04_results_"       , prefix3, ".html"))
 
-rmarkdown::render("01_tss_cpg_status.Rmd", output_file=paste0("01_tss_cpg_status_", prefix3, ".html"))
-rmarkdown::render("02_mapreduce_mean.Rmd", output_file=paste0("02_mapreduce_mean_", prefix3, ".html"))
-rmarkdown::render("03_da_GSE45332.Rmd"   , output_file=paste0("03_da_GSE45332_"   , prefix3, ".html"))
-rmarkdown::render("03_da_GSE5816.Rmd"    , output_file=paste0("03_da_GSE5816_"    , prefix3, ".html"))
-rmarkdown::render("04_results.Rmd"       , output_file=paste0("04_results_"       , prefix3, ".html"))
 
+nb_rnd_feat = 2000
+feature_pretreatment = "cen"
+prefix3 = paste0(feature_pretreatment, "_", nb_rnd_feat)
+rmarkdown::render("00_dmethr_pipeline.Rmd", output_file=paste0("00_dmethr_pipeline_", prefix3, ".html"))
 
-nb_rnd_feat = 0
+nb_rnd_feat = 2000
+feature_pretreatment = "raw"
+prefix3 = "centered"
+prefix3 = paste0(feature_pretreatment, "_", nb_rnd_feat)
+rmarkdown::render("00_dmethr_pipeline.Rmd", output_file=paste0("00_dmethr_pipeline_", prefix3, ".html"))
 
-rmarkdown::render("01_tss_cpg_status.Rmd")
-rmarkdown::render("02_mapreduce_mean.Rmd")
-rmarkdown::render("03_da_GSE45332.Rmd"   )
-rmarkdown::render("03_da_GSE5816.Rmd"    )
-rmarkdown::render("04_results.Rmd"       )
+# rmarkdown::render("01_tss_cpg_status.Rmd")
+# rmarkdown::render("02_mapreduce_mean.Rmd")
+# rmarkdown::render("03_da_GSE45332.Rmd"   )
+# rmarkdown::render("03_da_GSE5816.Rmd"    )
+# rmarkdown::render("04_results.Rmd"       )
 
 
 
