e1b14500518048a29046a4d13dc0684216336958
Florent Chuffart <florent.chuffart@gmail.com>
2021-06-17 10:08:36 +0200

    Screening parameters

--- results/00_run_pipeline.R
+++ results/00_run_pipeline.R
@@ -21,6 +21,21 @@ prefix3 = "centered"
 prefix3 = paste0(feature_pretreatment, "_", nb_rnd_feat)
 rmarkdown::render("00_dmethr_pipeline.Rmd", output_file=paste0("00_dmethr_pipeline_", prefix3, ".html"))
 
+
+ud_strs = c(2500, 1000, 500)
+feature_pretreatments = c("cen", "raw")
+
+
+for (ud_str in ud_strs) {
+  for (ud_str in ud_strs) {
+    nb_rnd_feat = 2000
+    feature_pretreatment = "raw"
+    prefix3 = "centered"
+    prefix3 = paste0(feature_pretreatment, "_", ud_str, "_", nb_rnd_feat)
+    rmarkdown::render("00_dmethr_pipeline.Rmd", output_file=paste0("00_dmethr_pipeline_", prefix3, ".html"))
+  }
+}
+
 # rmarkdown::render("01_tss_cpg_status.Rmd")
 # rmarkdown::render("02_mapreduce_mean.Rmd")
 # rmarkdown::render("03_da_GSE45332.Rmd"   )
