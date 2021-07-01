18a80c5c4709c0a3e0b2a232aed217423b0d4b5b
Florence Pittion <florence.pittion@gmail.com>
2021-07-01 15:57:40 +0200

    modifs run

--- results/00_run_pipeline.R
+++ results/00_run_pipeline.R
@@ -2,10 +2,19 @@
 tcga_projects = c("TCGA-LUSC", "TCGA-LUAD")
 
 nb_rnd_feat = 0
-ud_strs = c(2500, 1000, 500, 250)
+ud_strs = c(2500, 1000, 500, 250)[-1]
 # feature_pretreatments = c("cen", "raw")
 feature_pretreatments = c("raw")
 reducer_func2_names = c("mean", "max")
+gses = c("GSE45332", "GSE5816", "GSE14315", "GSE25427", "GSE22250")
+models = c(
+  "DNMT DKO vs. WT (ref.)", 
+  "5-aza 1000 nM vs. DMSO (ref.)",
+  "5-aza vs. Ctrl (ref.)",
+  "5-aza vs. Mock (ref.)",
+  "5-aza vs. WT (ref.)"
+)
+
 
 # Compute 00_dmethr_pipeline_XXX.html and feats_XXX.xlsx files
 for (feature_pretreatment in feature_pretreatments) {
@@ -14,7 +23,8 @@ for (feature_pretreatment in feature_pretreatments) {
     print(ud_str)
     for (reducer_func2_name in reducer_func2_names) {
       prefix3 = paste0(feature_pretreatment, "_", ud_str, "_", nb_rnd_feat, "_", reducer_func2_name)
-      rmarkdown::render("00_dmethr_pipeline.Rmd", output_file=paste0("00_dmethr_pipeline_", prefix3, ".html"))
+     
+     # rmarkdown::render("00_dmethr_pipeline.Rmd", output_file=paste0("00_dmethr_pipeline_", prefix3, ".html"))
     }
   }
 }
@@ -48,15 +58,21 @@ for (feature_pretreatment in feature_pretreatments) {
 
       for (gse in gses) {
         prefix3 = paste0(feature_pretreatment, "_", ud_str, "_", nb_rnd_feat, "_", reducer_func2_name)
-        print(prefix3)
-        feats = mread.xlsx (paste0("feats_", prefix3, ".xlsx"))
+        feats_filename = paste0("feats_", prefix3, ".xlsx")
+        print(feats_filename)
+        feats = mread.xlsx (feats_filename)
   
         gene_set = rownames(feats)[feats[["methplusplus"]]]
         expression_vector = feats[,paste0("l2fc_", gse)]
         names(expression_vector) = rownames(feats)
         expression_vector = expression_vector[!is.na(expression_vector)]
         utest = wilcox.test(expression_vector~ifelse(names(expression_vector)%in%gene_set, "methplusplus", "others"), las=2)
-        tmp_list[[paste0("utest_pval_", gse)]] = utestutest$p.value
+        pv = utest$p.value
+        print(pv)
+        tmp_list[[paste0("utest_pval_", gse)]] = pv
+        
+        
+        
       }
 
       
@@ -72,36 +88,36 @@ stats
 
 
 
-plot(stats$ud_str, -log10(stats$upval), col=as.numeric(as.factor(stats$gse)), pch=ifelse(stats$reducer_func2_name=="mean", 1, 2))
+#plot(stats$ud_str, -log10(stats$upval), col=as.numeric(as.factor(stats$gse)), pch=ifelse(stats$reducer_func2_name=="mean", 1, 2))
 
 
 
 
-beta_reg = c()
-
-layout(matrix(1:3, 1), respect=TRUE)
-for(ud_str in ud_strs){
-  
-    reducer_func2_name = "mean"
-    feature_pretreatment = "raw"
-    prefix3 = paste0(feature_pretreatment, "_", ud_str, "_", nb_rnd_feat, "_", reducer_func2_name)
-    featsout_raw = openxlsx::read.xlsx (paste0("feats_", prefix3, ".xlsx"))
-    feature_pretreatment = "cen"
-    prefix3 = paste0(feature_pretreatment, "_", ud_str, "_", nb_rnd_feat, "_", reducer_func2_name)
-    featsout_cen = openxlsx::read.xlsx (paste0("feats_", prefix3, ".xlsx"))
-    #plot(jitter(featsout_raw$nb_probes), jitter(featsout_cen$nb_probes), main=ud_str)
-    #abline(a=0, b=1)
-    
-    X = featsout_cen$nb_probes
-    Y = featsout_raw$nb_probes
-    reg = lm(X~Y)
-    plot(X, Y, main=ud_str, xlab="raw", ylab="center")
-    abline(reg, col = 2)
-    abline(a=0, b=1, lty=2, col="grey")
-    beta_reg = c(beta_reg, reg$coefficients[[2]])
-  
-}
-beta_reg
-
+# beta_reg = c()
+# 
+# layout(matrix(1:3, 1), respect=TRUE)
+# for(ud_str in ud_strs){
+#   
+#     reducer_func2_name = "mean"
+#     feature_pretreatment = "raw"
+#     prefix3 = paste0(feature_pretreatment, "_", ud_str, "_", nb_rnd_feat, "_", reducer_func2_name)
+#     featsout_raw = openxlsx::read.xlsx (paste0("feats_", prefix3, ".xlsx"))
+#     feature_pretreatment = "cen"
+#     prefix3 = paste0(feature_pretreatment, "_", ud_str, "_", nb_rnd_feat, "_", reducer_func2_name)
+#     featsout_cen = openxlsx::read.xlsx (paste0("feats_", prefix3, ".xlsx"))
+#     #plot(jitter(featsout_raw$nb_probes), jitter(featsout_cen$nb_probes), main=ud_str)
+#     #abline(a=0, b=1)
+#     
+#     X = featsout_cen$nb_probes
+#     Y = featsout_raw$nb_probes
+#     reg = lm(X~Y)
+#     plot(X, Y, main=ud_str, xlab="raw", ylab="center")
+#     abline(reg, col = 2)
+#     abline(a=0, b=1, lty=2, col="grey")
+#     beta_reg = c(beta_reg, reg$coefficients[[2]])
+#   
+# }
+# beta_reg
+# 
 
 
